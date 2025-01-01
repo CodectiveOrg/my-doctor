5e55641477c8cf2c5bb3e00344a6b64c64c07d12
BijanEisapour <bijanprogrammer@gmail.com>
2025-01-01 20:46:00 +0330

    [ADD] results

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,28 @@
 import { ReactElement } from "react";
 
+import CardComponent from "@/components/card/card.component";
+
+import styles from "./page.module.css";
+
+const items = Array(100)
+  .fill(null)
+  .map((_, i) => i + 1);
+
 export default function Page(): ReactElement {
-  return <div>جستجو</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.filters}>
+        <CardComponent>
+          <div className={styles.title}>زوج یا فرد</div>
+          <button>زوج</button>
+          <button>فرد</button>
+        </CardComponent>
+      </div>
+      <ul className={styles.results}>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
