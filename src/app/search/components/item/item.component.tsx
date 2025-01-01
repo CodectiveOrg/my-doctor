54a20c8cd4a94c048c48cae89a071f7bdd337367
BijanEisapour <bijanprogrammer@gmail.com>
2025-01-01 21:40:01 +0330

    [ADD] active item filter functionality

--- src/app/search/components/item/item.component.tsx
+++ src/app/search/components/item/item.component.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useMemo } from "react";
+
+import clsx from "clsx";
 
 import { FiltersContext } from "@/app/search/providers/filters/filters.provider";
 
@@ -13,5 +15,27 @@ type Props = {
 export default function ItemComponent({ item }: Props): ReactElement {
   const { filters } = useContext(FiltersContext);
 
-  return <li className={styles.item}>{item}</li>;
+  const isActive = useMemo(() => {
+    if (filters.even && item % 2 === 0) {
+      return true;
+    }
+
+    if (filters.odd && item % 2 === 1) {
+      return true;
+    }
+
+    if (filters.three && item % 3 === 0) {
+      return true;
+    }
+
+    if (filters.five && item % 5 === 0) {
+      return true;
+    }
+
+    return !!(filters.seven && item % 7 === 0);
+  }, [filters, item]);
+
+  return (
+    <li className={clsx(styles.item, isActive && styles.active)}>{item}</li>
+  );
 }
