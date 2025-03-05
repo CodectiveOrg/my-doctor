ae356fa7ecd7283b26476c84d3d440be892baea2
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-05 13:10:55 +0330

    [ADD] username and email validation

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -16,6 +16,25 @@ export async function POST(request: Request): Promise<ApiResponseType<null>> {
       return NextResponse.json({ error: parseError }, { status: 400 });
     }
 
+    let foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    if (foundUser) {
+      return NextResponse.json(
+        { error: "نام کاربری تکراری است." },
+        { status: 400 },
+      );
+    }
+
+    foundUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (foundUser) {
+      return NextResponse.json({ error: "ایمیل تکراری است." }, { status: 400 });
+    }
+
     await prisma.user.create({ data: body });
 
     return NextResponse.json({ data: null }, { status: 201 });
