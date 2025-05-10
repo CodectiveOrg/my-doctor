3b6e06d6323274b043b16c7bc8ea8d54555252ff
BijanProgrammer <bijaneisapour@gmail.com>
2025-05-10 15:01:28 +0330

    [ADD] dashboard layout

---
+++ src/app/dashboard/components/sidebar/sidebar.component.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+import styles from "./sidebar.module.css";
+
+export default function SidebarComponent(): ReactElement {
+  return <div className={styles.sidebar}></div>;
+}
